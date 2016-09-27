a6110429f5f6c97f8f88db3b92a8e7eb07e4b6cd
Harsh Singh <harsingh@cimpress.com>
2016-09-27 09:58:01 -0400

    Various build process changes
    Adds Sentry
    generate sourcemaps for css and js
    use yarn to manage libraries instead of lib for proper version support
    enable use strict
    update jquery 1.8 to 1.12, with jquery migrate in order to update bootstrap js
    simplify index.css by removing libraryish code like bootstrap, and animate.css
    update backbone, marked, underscore
    
    fix markdown, https, and sourcemap bug, extract out budicons.css
    
    removes unnecessary files in dist folder, fixes sourcemaps, optimizes scripts
    
    remove unused images, cleanup css, remove animate.css b/c unused
    
    remove unused gulpfile things for speed boost
    
    updates readme, and removes extra console.log

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,21 @@ var es = require('event-stream');
 var del = require('del');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var less = require('gulp-less');
+// var rename = require('gulp-rename');
+// var less = require('gulp-less');
 var gulpif = require('gulp-if');
 var handlebars = require('gulp-handlebars');
 var wrap = require('gulp-wrap');
-var declare = require('gulp-declare');
+var gulpDeclare = require('gulp-declare');
+var iife = require("gulp-iife");
 var watch = require('gulp-watch');
 var connect = require('gulp-connect');
-var header = require('gulp-header');
-var pkg = require('./package.json');
-var order = require('gulp-order');
+// var header = require('gulp-header');
+// var pkg = require('./package.json');
+// var order = require('gulp-order');
 var cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
+
 var banner = ['/**',
   ' * <%= pkg.name %> - <%= pkg.description %>',
   ' * @version v<%= pkg.version %>',
@@ -25,7 +28,7 @@ var banner = ['/**',
   ' */',
   ''].join('\n');
 
-var prod = true
+var prod = true;
 /**
  * Clean ups ./dist folder
  */
@@ -39,12 +42,16 @@ gulp.task('clean', function() {
 function templates() {
   return gulp
     .src(['./src/main/template/**/*'])
-    .pipe(handlebars())
+    .pipe(handlebars({
+      handlebars: require('handlebars')
+    }))
     .pipe(wrap('Handlebars.template(<%= contents %>)'))
-    .pipe(declare({
+    .pipe(gulpDeclare({
       namespace: 'Handlebars.templates',
       noRedeclare: true, // Avoid duplicate declarations
     }))
+    // .pipe(concat('swagger-ui-templates.js'))
+    // .pipe(gulp.dest('./dist'))
     .on('error', log);
 }
 
@@ -60,15 +67,13 @@ gulp.task('dist', ['clean'], function() {
       ]),
       templates()
     )
-    .pipe(order(['scripts.js', 'templates.js']))
-    .pipe(concat('swagger-ui.js'))
-    .pipe(wrap('(function(){<%= contents %>}).call(this);'))
-    .pipe(header(banner, { pkg: pkg } ))
-    .pipe(gulp.dest('./dist'))
-    .pipe(gulpif(prod, uglify()))
-    .on('error', log)
-    .pipe(rename({extname: '.min.js'}))
-    .on('error', log)
+    // .pipe(order(['scripts.js', 'templates.js']))
+    // .pipe(header(banner, { pkg: pkg } ))
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(iife({bindThis: true}))
+    .pipe(concat('swagger-ui.min.js'))
+    .pipe(gulpif(prod, uglify({mangle: false, compress: false}))).on('error', log)
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./dist'))
     .pipe(connect.reload());
 });
@@ -76,31 +81,31 @@ gulp.task('dist', ['clean'], function() {
 /**
  * Processes less files into CSS files
  */
-gulp.task('less', ['clean'], function() {
+// gulp.task('less', ['clean'], function() {
 
-  return gulp
-    .src([
-      './src/main/less/screen.less', // screen.less includes highlight_default.less, specs.less, and auth.less
-      './src/main/less/print.less',
-      './src/main/less/reset.less'
-    ])
-    .pipe(less())
-    .on('error', log)
-    .pipe(gulp.dest('./src/main/html/css/'))
-    .pipe(connect.reload());
-});
+//   return gulp
+//     .src([
+//       './src/main/less/screen.less', // screen.less includes highlight_default.less, specs.less, and auth.less
+//       './src/main/less/print.less',
+//       './src/main/less/reset.less'
+//     ])
+//     .pipe(less())
+//     .on('error', log)
+//     .pipe(gulp.dest('./src/main/css/'))
+//     .pipe(connect.reload());
+// });
 
 
 /**
  * Copy lib and html folders
  */
-gulp.task('copy', function() {
+gulp.task('copy', ['clean'], function() {
 
   // copy JavaScript files inside lib folder
-  gulp
-    .src(['./lib/**/*.{js,map}'])
-    .pipe(gulp.dest('./dist/lib'))
-    .on('error', log);
+  // gulp
+  //   .src(['./lib/**/*.{js,map}'])
+  //   .pipe(gulp.dest('./dist/lib'))
+  //   .on('error', log);
 
   // copy all files inside html folder
   gulp
@@ -112,18 +117,20 @@ gulp.task('copy', function() {
 /**
  * Concatenate and minify all CSS in the proper order
  */
-gulp.task('minify:css', function() {
+gulp.task('minify:css', ['clean'], function() {
   return gulp
     .src([
-      // './src/main/html/css/reset.css',  // api-explorer.css supplants reset.css
-      // './src/main/html/css/print.css',  // api-explorer.css supplants print.css
-      // './src/main/html/css/screen.css', // api-explorer.css supplants screen.css
-      './src/main/html/css/index.css',
-      './src/main/html/css/standalone.css',
-      './src/main/html/css/api-explorer.css'
+      // './src/main/css/reset.css',  // api-explorer.css supplants reset.css
+      // './src/main/css/print.css',  // api-explorer.css supplants print.css
+      // './src/main/css/screen.css', // api-explorer.css supplants screen.css
+      './src/main/css/index.css',
+      './src/main/css/standalone.css',
+      './src/main/css/api-explorer.css'
     ])
+    .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(concat('min.css'))
     .pipe(gulpif(prod, cleanCSS()))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./dist'))
     .on('error', log);
 });
@@ -131,24 +138,26 @@ gulp.task('minify:css', function() {
 /**
  * Concatenate and minify all JS in the proper order
  */
-gulp.task('minify:js', ['dist'], function() {
+gulp.task('minify:js', ['clean'], function() {
   return gulp
     .src([
       './lib/jquery.slideto.min.js',
       './lib/jquery.wiggle.min.js',
       './lib/jquery.ba-bbq.min.js',
-      './lib/handlebars-2.0.0.js',
-      './lib/underscore-min.js',
-      './lib/backbone-min.js',
+      './node_modules/handlebars/dist/handlebars.min.js',
+      './node_modules/underscore/underscore-min.js',
+      './node_modules/backbone/backbone-min.js',
+      './lib/backbone-migrate.js',
       './lib/highlight.7.3.pack.js',
-      './lib/marked.js',
+      './node_modules/marked/marked.min.js',
       './lib/swagger-oauth.js',
-      './lib/bootstrap.min.js',
-      './lib/clipboard.min.js',
+      './node_modules/clipboard/dist/clipboard.min.js',
       './lib/copyToClipboard.js',
     ])
+    .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(concat('min.js'))
     .pipe(gulpif(prod, uglify()))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./dist'))
     .on('error', log);
 });
