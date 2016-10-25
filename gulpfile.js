42eef1ad205f84ad6bd5547336135e2c311e8fdc
Harsh Singh <harsingh@cimpress.com>
2016-10-25 17:19:47 -0400

    PR changes
    make mobile-nav drowdown display more title
    
    reintroduces fonts.css

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,6 @@ var es = require('event-stream');
 var del = require('del');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-// var rename = require('gulp-rename');
-// var less = require('gulp-less');
 var gulpif = require('gulp-if');
 var handlebars = require('gulp-handlebars');
 var wrap = require('gulp-wrap');
@@ -14,24 +12,11 @@ var gulpDeclare = require('gulp-declare');
 var iife = require("gulp-iife");
 var watch = require('gulp-watch');
 var connect = require('gulp-connect');
-// var header = require('gulp-header');
-// var pkg = require('./package.json');
-// var order = require('gulp-order');
 var cleanCSS = require('gulp-clean-css');
 var sourcemaps = require('gulp-sourcemaps');
 
-var banner = ['/**',
-  ' * <%= pkg.name %> - <%= pkg.description %>',
-  ' * @version v<%= pkg.version %>',
-  ' * @link <%= pkg.homepage %>',
-  ' * @license <%= pkg.license %>',
-  ' */',
-  ''].join('\n');
-
 var prod = true;
-/**
- * Clean ups ./dist folder
- */
+
 gulp.task('clean', function() {
   return del.sync(['./dist/**']);
 });
@@ -50,8 +35,6 @@ function templates() {
       namespace: 'Handlebars.templates',
       noRedeclare: true, // Avoid duplicate declarations
     }))
-    // .pipe(concat('swagger-ui-templates.js'))
-    // .pipe(gulp.dest('./dist'))
     .on('error', log);
 }
 
@@ -59,7 +42,6 @@ function templates() {
  * Build a distribution
  */
 gulp.task('dist', ['clean'], function() {
-
   return es.merge(
       gulp.src([
         './src/main/javascript/**/*.js',
@@ -67,8 +49,6 @@ gulp.task('dist', ['clean'], function() {
       ]),
       templates()
     )
-    // .pipe(order(['scripts.js', 'templates.js']))
-    // .pipe(header(banner, { pkg: pkg } ))
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(iife({bindThis: true}))
     .pipe(concat('swagger-ui.min.js'))
@@ -79,39 +59,33 @@ gulp.task('dist', ['clean'], function() {
 });
 
 /**
- * Processes less files into CSS files
- */
-// gulp.task('less', ['clean'], function() {
-
-//   return gulp
-//     .src([
-//       './src/main/less/screen.less', // screen.less includes highlight_default.less, specs.less, and auth.less
-//       './src/main/less/print.less',
-//       './src/main/less/reset.less'
-//     ])
-//     .pipe(less())
-//     .on('error', log)
-//     .pipe(gulp.dest('./src/main/css/'))
-//     .pipe(connect.reload());
-// });
-
-
-/**
  * Copy lib and html folders
  */
 gulp.task('copy', ['clean'], function() {
-
-  // copy JavaScript files inside lib folder
-  // gulp
-  //   .src(['./lib/**/*.{js,map}'])
-  //   .pipe(gulp.dest('./dist/lib'))
-  //   .on('error', log);
-
-  // copy all files inside html folder
-  gulp
-    .src(['./src/main/html/**/*'])
+  gulp.src([
+      './node_modules/jquery/dist/jquery.min.js',
+      './node_modules/jquery-migrate/dist/jquery-migrate.min.js',
+      './node_modules/bootstrap/dist/js/bootstrap.min.js',
+      './lib/jquery.slideto.min.js',
+      './lib/jquery.wiggle.min.js',
+      './lib/jquery.ba-bbq.min.js',
+      './node_modules/handlebars/dist/handlebars.min.js',
+      './node_modules/underscore/underscore-min.js',
+      './node_modules/backbone/backbone-min.js',
+      './lib/highlight.7.3.pack.js',
+      './node_modules/marked/marked.min.js',
+      './node_modules/clipboard/dist/clipboard.min.js',
+    ])
+    .pipe(concat('lib.js'))
     .pipe(gulp.dest('./dist'))
-    .on('error', log);
+  gulp.src([
+      './node_modules/bootstrap/dist/css/bootstrap.min.css',
+      './src/main/html/css/budicon.fixed.css',
+    ])
+    .pipe(concat('lib.css'))
+    .pipe(gulp.dest('./dist'))
+  gulp.src(['./src/main/html/*.html','./src/main/html/*.json', './src/main/html/images/**'], { "base" : "./src/main/html" })
+    .pipe(gulp.dest('./dist'))
 });
 
 /**
@@ -120,12 +94,12 @@ gulp.task('copy', ['clean'], function() {
 gulp.task('minify:css', ['clean'], function() {
   return gulp
     .src([
-      // './src/main/css/reset.css',  // api-explorer.css supplants reset.css
-      // './src/main/css/print.css',  // api-explorer.css supplants print.css
-      // './src/main/css/screen.css', // api-explorer.css supplants screen.css
-      './src/main/css/index.css',
-      './src/main/css/standalone.css',
-      './src/main/css/api-explorer.css'
+      // './src/main/html/css/reset.css',  // api-explorer.css supplants reset.css
+      // './src/main/html/css/print.css',  // api-explorer.css supplants print.css
+      // './src/main/html/css/screen.css', // api-explorer.css supplants screen.css
+      './src/main/html/css/index.css',
+      './src/main/html/css/standalone.css',
+      './src/main/html/css/api-explorer.css'
     ])
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(concat('min.css'))
@@ -141,17 +115,8 @@ gulp.task('minify:css', ['clean'], function() {
 gulp.task('minify:js', ['clean'], function() {
   return gulp
     .src([
-      './lib/jquery.slideto.min.js',
-      './lib/jquery.wiggle.min.js',
-      './lib/jquery.ba-bbq.min.js',
-      './node_modules/handlebars/dist/handlebars.min.js',
-      './node_modules/underscore/underscore-min.js',
-      './node_modules/backbone/backbone-min.js',
       './lib/backbone-migrate.js',
-      './lib/highlight.7.3.pack.js',
-      './node_modules/marked/marked.min.js',
       './lib/swagger-oauth.js',
-      './node_modules/clipboard/dist/clipboard.min.js',
       './lib/copyToClipboard.js',
     ])
     .pipe(sourcemaps.init({loadMaps: true}))
@@ -188,4 +153,4 @@ function log(error) {
 
 
 gulp.task('default', ['dist', 'copy', 'minify:js', 'minify:css']);
-gulp.task('serve', ['connect', 'watch']);
+gulp.task('serve', ['connect', 'watch', 'default']);
